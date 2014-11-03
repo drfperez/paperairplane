2fe2e070c5f01b2ab30b5660bbbb64a0dfbd4022
Francisco Pérez García <drfperez@gmail.com>
2014-11-03 19:48:20 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,13 +1,41 @@
-function speed()
+    function speed1()
         {
             // Obtain the current values of distance and time.
-            var distance = new Number(
-                document.getElementById("distance").value);
-            var time = new Number(
-                document.getElementById("time").value);
+            var distance1 = new Number(
+                document.getElementById("distance1").value);
+            var time1 = new Number(
+                document.getElementById("time1").value);
             
             // Set the result to reflect the division of the two numbers.
-            document.getElementById("speed").setAttribute(
-                "value", distance / time);
-        }        
+            document.getElementById("speed1").setAttribute(
+                "value", distance1/time1);
+        }      
+      function speed2()
+        {
+            // Obtain the current values of distance and time.
+            var distance2 = new Number(
+                document.getElementById("distance2").value);
+            var time2 = new Number(
+                document.getElementById("time2").value);
+            
+            // Set the result to reflect the division of the two numbers.
+            document.getElementById("speed2").setAttribute(
+                "value", distance2 / time2);
+        }  function speedmean()
+        {
+           var distance1 = new Number(
+                document.getElementById("distance1").value);
+            var time1 = new Number(
+                document.getElementById("time1").value);
+            var distance2 = new Number(
+                document.getElementById("distance2").value);
+            var time2 = new Number(
+                document.getElementById("time2").value);
+            
+
+
+            // Set the result to reflect the mean of the two numbers.
+            document.getElementById("speedmean").setAttribute("value", ((distance1/time1)+(distance2/time2))/2);
+
+        }       
     
